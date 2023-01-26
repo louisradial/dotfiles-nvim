d076387c56afbc35da7a107f2ddae3d162159421
louis radial <louis.radial@usp.br>
2023-01-26 14:11:08 -0300

    fixed cmp

--- lua/radial/plugins/cmp.lua
+++ lua/radial/plugins/cmp.lua
@@ -23,7 +23,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ["<Left>"] = cmp.mapping.scroll_docs(-4),
                 ["<Right>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
+                -- ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
